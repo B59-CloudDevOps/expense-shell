1df8ced06f0c35e13aa1655a9597a66ccf92cda4
CodingManoj <CodingManoj@gmail.com>
2025-03-10 21:07:25 -0400

    Bash Scripting - Expense Automation~ AutoPush

--- frontend.sh
+++ frontend.sh
@@ -3,11 +3,13 @@
 component=frontend
 logFile=/tmp/$component.log
 # Common function to print the status of the component
-if [ $1 -eq 0 ]; then 
-    echo -e "\e[32m Success \e[0m"    
-else
-    echo -e "\e[31m Failure \e[0m"    
-fi
+stat() {
+    if [ $1 -eq 0 ]; then 
+        echo -e "\e[32m Success \e[0m"    
+    else
+        echo -e "\e[31m Failure \e[0m"    
+    fi
+}
 
 echo -n "Installing Nginx:"
 dnf install nginx -y     &>> $logFile
