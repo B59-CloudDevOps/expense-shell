6ee9005e8586bc953e571a8c0cc0bfb6b6fd070a
CodingManoj <CodingManoj@gmail.com>
2025-03-10 20:53:52 -0400

    Bash Scripting - Expense Automation~ AutoPush

--- frontend.sh
+++ frontend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "Installing Nginx:"
+echo -n "Installing Nginx:"
 dnf install nginx -y     &>> /tmp/frontend.log
 if [ $? -eq 0 ]; then 
     echo -e "\e[32m Success \e[0m"    
@@ -8,7 +8,7 @@ else
     echo -e "\e[31m Failure \e[0m"    
 fi
 
-echo -e "Starting Nginx:"
+echo -n "Starting Nginx:"
 systemctl enable nginx   &>> /tmp/frontend.log
 systemctl start nginx    &>> /tmp/frontend.log
 if [ $? -eq 0 ]; then 
