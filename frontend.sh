dae05e856f99b1bbc6c7dc7c835ba88275303490
CodingManoj <CodingManoj@gmail.com>
2025-03-10 21:06:13 -0400

    Bash Scripting - Expense Automation~ AutoPush

--- frontend.sh
+++ frontend.sh
@@ -2,49 +2,38 @@
 
 component=frontend
 logFile=/tmp/$component.log
-echo -n "Installing Nginx:"
-dnf install nginx -y     &>> $logFile
-if [ $? -eq 0 ]; then 
+# Common function to print the status of the component
+if [ $1 -eq 0 ]; then 
     echo -e "\e[32m Success \e[0m"    
 else
     echo -e "\e[31m Failure \e[0m"    
 fi
 
+echo -n "Installing Nginx:"
+dnf install nginx -y     &>> $logFile
+stat $?
+
+
 echo -n "Starting Nginx:"
 systemctl enable nginx   &>> $logFile
 systemctl start nginx    &>> $logFile
-if [ $? -eq 0 ]; then 
-    echo -e "\e[32m Success \e[0m"
-else
-    echo -e "\e[31m Failure \e[0m"
-fi
+stat $?
 
 echo -n "Clearning Old $component Content:"
 rm -rf /usr/share/nginx/html/* 
-if [ $? -eq 0 ]; then 
-    echo -e "\e[32m Success \e[0m"
-else
-    echo -e "\e[31m Failure \e[0m"
-fi
+stat $?
 
 echo -n "Downloading $component Content:"
 curl -o /tmp/$component.zip https://expense-web-app.s3.amazonaws.com/frontend.zip &>> $logFile
-if [ $? -eq 0 ]; then 
-    echo -e "\e[32m Success \e[0m"
-else
-    echo -e "\e[31m Failure \e[0m"
-fi
+stat $?
 
 echo -n "Extracting $component Content:"
 cd /usr/share/nginx/html 
 unzip -o /tmp/$component.zip &>> $logFile
-if [ $? -eq 0 ]; then 
-    echo -e "\e[32m Success \e[0m"
-else
-    echo -e "\e[31m Failure \e[0m"
-fi
+stat $?
 
 echo -n "Restarting Nginx:"
 systemctl restart nginx 
+stat $?
 
-echo -n " *****  frontend Execution Completed  *****"
+echo -n "*****  $component Execution Completed  *****"
