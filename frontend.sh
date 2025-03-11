69d31cf6269611c2a071738f938448c7b9b27c91
CodingManoj <CodingManoj@gmail.com>
2025-03-10 21:15:28 -0400

    Bash Scripting - Expense Automation~ AutoPush

--- frontend.sh
+++ frontend.sh
@@ -5,7 +5,7 @@ logFile=/tmp/$component.log
 # Common function to print the status of the component
 if [ $(id -u) -ne 0 ]; then 
     echo -e "\e[31m You should be root user to perform this script \e[0m"
-    echo -e "Example usage: \n\t \e[31m sudo bash $0 \e[0m"
+    echo -e "Example usage: \n\t \e[35m sudo bash $0 \e[0m"
     exit 2
 fi
 
