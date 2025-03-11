72314d9ec9ba2c222e7bbad2c6513b81bc416f46
CodingManoj <CodingManoj@gmail.com>
2025-03-10 20:56:37 -0400

    Bash Scripting - Expense Automation~ AutoPush

--- frontend.sh
+++ frontend.sh
@@ -17,9 +17,32 @@ else
     echo -e "\e[31m Failure \e[0m"
 fi
 
-# rm -rf /usr/share/nginx/html/* 
-# curl -o /tmp/frontend.zip https://expense-web-app.s3.amazonaws.com/frontend.zip
-# cd /usr/share/nginx/html 
-# unzip /tmp/frontend.zip
-# systemctl restart nginx 
-# set-hostname frontend
+echo -n "Clearning Old Frontend Content:"
+rm -rf /usr/share/nginx/html/* 
+if [ $? -eq 0 ]; then 
+    echo -e "\e[32m Success \e[0m"
+else
+    echo -e "\e[31m Failure \e[0m"
+fi
+
+echo -n "Downloading Frontend Content:"
+curl -o /tmp/frontend.zip https://expense-web-app.s3.amazonaws.com/frontend.zip
+if [ $? -eq 0 ]; then 
+    echo -e "\e[32m Success \e[0m"
+else
+    echo -e "\e[31m Failure \e[0m"
+fi
+
+echo -n "Extracting Frontend Content:"
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip &>> /tmp/frontend.log
+if [ $? -eq 0 ]; then 
+    echo -e "\e[32m Success \e[0m"
+else
+    echo -e "\e[31m Failure \e[0m"
+fi
+
+echo -n "Restarting Nginx:"
+systemctl restart nginx 
+
+echo -n " *****  Frontend Execution Completed  *****"
