8d6c999efab273ff73b8fda8ed846d5ef685c33d
CodingManoj <CodingManoj@gmail.com>
2025-03-12 20:54:23 -0400

    Bash Scripting - Expense Automation~ AutoPush

--- 2-mysql.sh
+++ 2-mysql.sh
@@ -9,6 +9,13 @@ if [ $(id -u) -ne 0 ]; then
     exit 2
 fi
 
+if [ -z $1 ]; then 
+    echo -e "\e[31m Please provide the password to set for root user \e[0m"
+    echo -e "Example usage: \n\t \e[35m sudo bash $0 password \e[0m"
+    exit 1
+fi
+ 
+
 stat() {
     if [ $1 -eq 0 ]; then 
         echo -e "\e[32m Success \e[0m"    
